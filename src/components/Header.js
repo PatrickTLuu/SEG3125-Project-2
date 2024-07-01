b67ab3dcd78595271c6b3cffa63a0aa5a00d3fd0
PatrickTLuu <pluu079@uottawa.ca>
2024-07-01 10:23:00 -0400

    Implement router into nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,18 @@ import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../css/Header.css"
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     const [showTournaments, setShowTournaments] = useState(false);
     const [showTrades, setShowTrades] = useState(false);
 
 
-    const showTournamentsDropdown = (e) => {
-        setShowTournaments(!showTournaments);
-    }
-    const hideTournamentsDropdown = (e) => {
-        setShowTournaments(false);
-    }
+    const showTournamentsDropdown = () => setShowTournaments(true);
+    const hideTournamentsDropdown = () => setShowTournaments(false);
 
-    const showTradesDropdown = (e) => {
-        setShowTrades(!showTrades);
-    }
-    const hideTradesDropdown = (e) => {
-        setShowTrades(false);
-    }
+    const showTradesDropdown = () => setShowTrades(true);
+    const hideTradesDropdown = () => setShowTrades(false);
 
     return (
         <div>
@@ -30,18 +23,18 @@ export default function Header() {
                 <h2 id="brand-name">Games Central</h2>
             </Row>
             <Row>
-                <Navbar className="justify-content-center" bg="dark" data-bs-theme="dark">
-                    <Col>
-                        <NavDropdown title="Tournaments" className="text-align-center" show={showTournaments} onMouseEnter={showTournamentsDropdown} onMouseLeave={hideTournamentsDropdown}>
-                            <NavDropdown.Item className="dropdown-item">Create a tournament</NavDropdown.Item>
-                            <NavDropdown.Item className="dropdown-item">Join a tournament</NavDropdown.Item>
+                <Navbar className="justify-content-center navigation-bar" bg="dark" data-bs-theme="dark">
+                    <Col onMouseEnter={showTournamentsDropdown} onMouseLeave={hideTournamentsDropdown}>
+                        <NavDropdown title="Tournaments" className="text-align-center" show={showTournaments}>
+                            <NavDropdown.Item className="dropdown-item" as={Link} to="create_tournament">Create a tournament</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item" as={Link} to="view_tournaments">View tournaments</NavDropdown.Item>
                         </NavDropdown>
                         <span className="rectangle blue"></span>
                     </Col>
-                    <Col>
-                    <NavDropdown title="Trades" className="text-align-center" show={showTrades} onMouseEnter={showTradesDropdown} onMouseLeave={hideTradesDropdown}>
-                        <NavDropdown.Item className="dropdown-item">View open trade requests</NavDropdown.Item>
-                        <NavDropdown.Item className="dropdown-item">Post a trade request</NavDropdown.Item>
+                    <Col onMouseEnter={showTradesDropdown} onMouseLeave={hideTradesDropdown}>
+                    <NavDropdown title="Trades" className="text-align-center" show={showTrades}>
+                        <NavDropdown.Item className="dropdown-item" as={Link} to="post_trade">Post a trade request</NavDropdown.Item>
+                        <NavDropdown.Item className="dropdown-item" as={Link} to="view_trades">View trades</NavDropdown.Item>
                     </NavDropdown>
                     <span className="rectangle green"></span>
                     </Col>
