656f756ed76023ae51e6506a02fa488b9565bc55
PatrickTLuu <pluu079@uottawa.ca>
2024-07-15 19:39:07 -0400

    Add validation to all but 1 input

--- src/pages/PostTrade.js
+++ src/pages/PostTrade.js
@@ -3,21 +3,29 @@ import Multiselect from '../components/Multiselect'
 import Header from '../components/Header'
 import "../index.css"
 import "../css/pages/PostTrade.css"
+import { useForm } from "react-hook-form";
 
 export default function CreateTournament() {
+    const { register, control, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
         <div>
             <Header></Header>
             <Container>
                 <h3 className="title">Post a trade</h3>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group className="margin-top">
                         <Form.Label>Contact email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter your email"></Form.Control>
+                        <Form.Control type="email" placeholder="example@example.com" {...register("email", { required: true })}></Form.Control>
+                        {errors.email && <Form.Text>Please enter your email</Form.Text>}
                     </Form.Group>
                     <Form.Group className="margin-top">
                         <Form.Label>Video Game</Form.Label>
-                        <Form.Control type="text" placeholder="Enter video game"></Form.Control>
+                        <Form.Control type="text" placeholder="Enter a video game" {...register("videoGame", { required: true })}></Form.Control>
+                        {errors.videoGame && <Form.Text>Please enter your video game</Form.Text>}
                     </Form.Group>
                     <Row className="margin-top">
                         <Col>
@@ -29,7 +37,8 @@ export default function CreateTournament() {
                         <Col>
                             <Form.Group>
                                 <Form.Label>Expiry Date</Form.Label>
-                                <Form.Control className="date-input" type="date"></Form.Control>
+                                <Form.Control className="date-input" type="date" {...register("date", { required: true })}></Form.Control>
+                                {errors.date && <Form.Text>Please enter an expiry name</Form.Text>}
                             </Form.Group>
                         </Col>
                     </Row>
@@ -37,17 +46,21 @@ export default function CreateTournament() {
                         <Col>
                             <Form.Group>
                                 <Form.Label>City</Form.Label>
-                                <Form.Control type="text" placeholder="Enter city"></Form.Control>
+                                <Form.Control type="text" placeholder="Enter a city" {...register("city", { required: true })}></Form.Control>
+                                {errors.city && <Form.Text>Please enter your city</Form.Text>}
                             </Form.Group>
                         </Col>
                     </Row>
                     <Form.Group className="margin-top">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea"></Form.Control>
+                        <Form.Control as="textarea" {...register("description", { required: true })}></Form.Control>
+                        {errors.description && <Form.Text>Please enter a description</Form.Text>}
                     </Form.Group>
                     <Form.Group className="margin-top" controlId="formFile">
                         <Form.Label>Upload an image</Form.Label>
-                        <Form.Control type="file" />
+                        <Form.Control type="file" {...register("file", { required: true })}></Form.Control>
+                        {errors.file && <Form.Text>Please upload an image</Form.Text>}
+
                     </Form.Group>
                     <Row className="margin-top text-align-center">
                         <Col>
